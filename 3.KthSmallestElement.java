16959e1ca0f08dd0dddb7e64b319eee9ecae4c23
Jaineel-Mamtora <jmamtora99@gmail.com>
2021-01-04 21:07:59 +0530

    added: Kth Smallest Element solution

---
+++ 3.KthSmallestElement.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class kthSmallestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+            int k = sc.nextInt();
+            System.out.println(kthSmallest(arr, 0, n - 1, k));
+        }
+        sc.close();
+    }
+
+    public static int kthSmallest(int[] arr, int l, int r, int k) {
+        Arrays.sort(arr);
+        return arr[k - 1];
+    }
+}
